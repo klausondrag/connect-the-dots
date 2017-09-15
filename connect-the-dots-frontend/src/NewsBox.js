a6cd9c51094857f2d91073fbb8cbb4cc43dd9c7e
Gott50 <xxGott50xx@gmail.com>
2017-09-16 00:49:21 +0200

    add Thumbnail to NewsBox

--- connect-the-dots-frontend/src/NewsBox.js
+++ connect-the-dots-frontend/src/NewsBox.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Thumbnail} from "react-bootstrap";
 
 class Feed extends Component {
-  render() {
-    return (
-      <div className="Feed">
-        news
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Feed">
+                <Thumbnail src="/assets/img/logo-sueddeutsche-zeitung.png"  alt="Sueddeutsche Zeitung">
+                    <h3>HeadLine</h3>
+                    <p>text</p>
+                </Thumbnail>
+            </div>
+        );
+    }
 }
 
 export default Feed;
