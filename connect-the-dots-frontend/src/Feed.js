1ffbdc440781cdac1d5dd7b04b471d8725287784
Gott50 <xxGott50xx@gmail.com>
2017-09-16 01:36:18 +0200

    add Dummy Feed

--- connect-the-dots-frontend/src/Feed.js
+++ connect-the-dots-frontend/src/Feed.js
@@ -2,11 +2,54 @@ import React, {Component} from 'react';
 import Item from "./Item";
 
 class Feed extends Component {
+    constructor(props, context) {
+        super(props, context);
+        let news = [
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+            {
+                logo: "/assets/img/logo-sueddeutsche-zeitung.png",
+                headline: "headline",
+                excerpt: "text",
+            },
+        ];
+        this.state = {
+            feed: [
+                news, news, news
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="Feed">
                 <div className="container">
-                    <Item/>
+                    {this.state.feed.map(news =>
+                        <Item news={news}/>
+                    )}
                 </div>
             </div>
         );
