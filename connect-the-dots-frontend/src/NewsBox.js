866e5bdcd2b188495c1550b283927560de7ad99c
Gott50 <xxGott50xx@gmail.com>
2017-09-16 00:29:15 +0200

    add NewsBox

--- connect-the-dots-frontend/src/NewsBox.js
+++ connect-the-dots-frontend/src/NewsBox.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import NewsBox from "./NewsBox";
 
 class Feed extends Component {
   render() {
     return (
       <div className="Feed">
-        <NewsBox/>
+        news
       </div>
     );
   }
