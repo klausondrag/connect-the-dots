54d9ca8c7428950933af19993aa7f8070aa9405e
Gott50 <xxGott50xx@gmail.com>
2017-09-16 00:26:07 +0200

    add Feed

---
+++ connect-the-dots-frontend/src/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feed extends Component {
+  render() {
+    return (
+      <div className="Feed">
+        feed
+      </div>
+    );
+  }
+}
+
+export default Feed;
