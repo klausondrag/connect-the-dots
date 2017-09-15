6a437642d9e517d59c6bd8eb2508609018b0381e
Gott50 <xxGott50xx@gmail.com>
2017-09-16 01:40:45 +0200

    add NavBar

--- connect-the-dots-frontend/src/App.js
+++ connect-the-dots-frontend/src/App.js
@@ -1,15 +1,23 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import Feed from "./Feed";
 import './App.css';
+import {Navbar} from "react-bootstrap";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Feed/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/#">Connect the Dots</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                </Navbar>
+                <Feed/>
+            </div>
+        );
+    }
 }
 
 export default App;
