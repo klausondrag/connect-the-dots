54dc0b710761f45c6e35d18605f30cfb4fedcd41
Gott50 <xxGott50xx@gmail.com>
2017-09-16 01:30:56 +0200

    add container

--- connect-the-dots-frontend/src/Feed.js
+++ connect-the-dots-frontend/src/Feed.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Item from "./Item";
 
 class Feed extends Component {
-  render() {
-    return (
-      <div className="Feed">
-        <Item/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Feed">
+                <div className="container">
+                    <Item/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Feed;
