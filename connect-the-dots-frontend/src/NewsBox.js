ebd3ed0edb8f9b2656c79d3ba62b24f218c02407
Gott50 <xxGott50xx@gmail.com>
2017-09-16 00:50:38 +0200

    add Col md=2

--- connect-the-dots-frontend/src/NewsBox.js
+++ connect-the-dots-frontend/src/NewsBox.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import {Thumbnail} from "react-bootstrap";
+import {Thumbnail, Col} from "react-bootstrap";
 
 class Feed extends Component {
     render() {
         return (
             <div className="Feed">
-                <Thumbnail src="/assets/img/logo-sueddeutsche-zeitung.png"  alt="Sueddeutsche Zeitung">
-                    <h3>HeadLine</h3>
-                    <p>text</p>
-                </Thumbnail>
+                <Col md={2}>
+                    <Thumbnail src="/assets/img/logo-sueddeutsche-zeitung.png" alt="Sueddeutsche Zeitung">
+                        <h3>HeadLine</h3>
+                        <p>text</p>
+                    </Thumbnail>
+                </Col>
             </div>
         );
     }
