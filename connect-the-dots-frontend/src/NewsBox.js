bdaf2916863d8e0bf71ab6551b71f0748962b25e
vauhochzett <vauhochzett@users.noreply.github.com>
2017-09-16 02:59:36 +0200

    Modifies items to adhere to basic format

--- connect-the-dots-frontend/src/NewsBox.js
+++ connect-the-dots-frontend/src/NewsBox.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
-import {Thumbnail} from "react-bootstrap";
+import {Panel, Media, Label} from "react-bootstrap";
 
 class NewsBox extends Component {
     render() {
         return (
-            <div className="NewsBox">
-                    <Thumbnail src={this.props.news.image}>
-                        <h3>{this.props.news.headline}</h3>
-                        <code>{this.props.news.source}</code>
-                        <p>{this.props.news.excerpt}</p>
-                    </Thumbnail>
-            </div>
+            <Panel>
+                <Media md={6}>
+                    <Media.Left>
+                        <img width={64} height={64} src={this.props.news.image_url} alt="Image"/>
+                    </Media.Left>
+                    <Media.Body>
+                        <Media.Heading>{this.props.news.headline}</Media.Heading>
+                        <p><Label bsStyle="default">{this.props.news.source}</Label> {this.props.news.excerpt}</p>
+                    </Media.Body>
+                </Media>
+            </Panel>
         );
     }
 }
