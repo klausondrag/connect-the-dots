f3a01ff60d052357da5635905b3f26bf7c6f2a3e
vauhochzett <vauhochzett@users.noreply.github.com>
2017-09-16 04:32:47 +0200

    Adds surrounding div and custom class to NewsBox

--- connect-the-dots-frontend/src/NewsBox.js
+++ connect-the-dots-frontend/src/NewsBox.js
@@ -4,17 +4,19 @@ import {Panel, Media, Label} from "react-bootstrap";
 class NewsBox extends Component {
     render() {
         return (
-            <Panel>
-                <Media md={6}>
-                    <Media.Left>
-                        <img width={64} height={64} src={this.props.news.image_url} alt="Image"/>
-                    </Media.Left>
-                    <Media.Body>
-                        <Media.Heading>{this.props.news.headline}</Media.Heading>
-                        <p><Label bsStyle="default">{this.props.news.source}</Label> {this.props.news.excerpt}</p>
-                    </Media.Body>
-                </Media>
-            </Panel>
+            <div className="NewsBox">
+                <Panel className="news-box-panel">
+                    <Media md={6}>
+                        <Media.Left>
+                            <img width={64} height={64} src={this.props.news.image_url} alt="Image"/>
+                        </Media.Left>
+                        <Media.Body>
+                            <Media.Heading>{this.props.news.headline}</Media.Heading>
+                            <p><Label bsStyle="default">{this.props.news.source}</Label> {this.props.news.excerpt}</p>
+                        </Media.Body>
+                    </Media>
+                </Panel>
+            </div>
         );
     }
 }
