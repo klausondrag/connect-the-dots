86d1eba546ca87d56d6cb574a898c3d87b57a990
Gott50 <xxGott50xx@gmail.com>
2017-09-16 05:27:39 +0200

    add getFeed()

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -4,19 +4,8 @@ import Item from "./Item";
 class Feed extends Component {
     constructor(props, context) {
         super(props, context);
-        let item = {
-            source: "Fox News",
-            image_url: "/assets/img/london.jpg",
-            headline: "headline",
-            excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur massa quis aliquam blandit. Pellentesque quis lectus felis. Praesent consequat libero ac lobortis tincidunt. Sed ut tortor nec sapien dignissim varius ut ut est."
-        };
-        let news = [
-            item, item, item, item,
-        ];
         this.state = {
-            feed: [
-                news, news, news
-            ]
+            feed: this.getFeed()
         }
     }
 
@@ -26,13 +15,21 @@ class Feed extends Component {
                 <div className="container">
                     <ul className="items-list">
                         {this.state.feed.map(news =>
-                            <li> <Item news={news}/> </li>
+                            <li><Item news={news}/></li>
                         )}
                     </ul>
                 </div>
             </div>
         );
     }
+
+    getFeed() {
+        return fetch(`/api`, {
+            accept: 'application/json',
+        }).then(checkStatus)
+            .then(parseJSON);
+    }
+
 }
 
 export default Feed;
