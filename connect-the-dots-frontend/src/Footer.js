bd1e647f765d2a643901039d08150e69793f825e
vauhochzett <vauhochzett@users.noreply.github.com>
2017-09-16 04:33:50 +0200

    Adds basic footer

---
+++ connect-the-dots-frontend/src/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>
+                    Footer
+                </p>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
