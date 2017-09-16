72810a17ccb4b31d7d6b4506d8babe4b3dcd5a57
Gott50 <xxGott50xx@gmail.com>
2017-09-16 12:45:21 +0200

    move SourceName Label

--- client/src/NewsBox.js
+++ client/src/NewsBox.js
@@ -9,11 +9,13 @@ class NewsBox extends Component {
                 <Panel className="news-box-panel">
                     <Media md={6}>
                         <Media.Left>
+                            <Label bsStyle="default">{this.props.news.name}</Label>
+                            <hr/>
                             <img width={64} height={64} src={this.props.news.image_url} alt="Image"/>
                         </Media.Left>
                         <Media.Body>
                             <Media.Heading>{this.props.news.headline}</Media.Heading>
-                            <p><Label bsStyle="default">{this.props.news.name}</Label> {this.props.news.excerpt}</p>
+                            <p> {this.props.news.excerpt}</p>
                         </Media.Body>
                     </Media>
                 </Panel>
